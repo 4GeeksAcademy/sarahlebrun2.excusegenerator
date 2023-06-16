f0f761f1906feabb2b4b7d0b8cad4b177aaa2b55
Sarahlebrun2 <130923257+Sarahlebrun2@users.noreply.github.com>
2023-06-16 14:41:30 +0000

    update h1

--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = generateExcuse();
+  });
+};
+
+let generateExcuse = () => {
+  let pronoun = ["a", "the", "that"];
+  let subject = ["kid", "cat", "brid"];
+  let action = ["eat my", "shredded my", "ran over my"];
+  let stuff = ["homework", "food", "newspaper"];
+
+  let proIndex = Math.floor(Math.random() * pronoun.length);
+  let subIndex = Math.floor(Math.random() * subject.length);
+  let actIndex = Math.floor(Math.random() * action.length);
+  let stuIndex = Math.floor(Math.random() * stuff.length);
+
+  return (
+    pronoun[proIndex] +
+    " " +
+    subject[subIndex] +
+    " " +
+    action[actIndex] +
+    " " +
+    stuff[stuIndex] +
+    " "
+  );
 };
